91a709571f0407e58eb88bea2aba8346283a1f9c
Yahya HUSSEIN <husain.host@gmail.com>
2018-05-26 12:33:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+
+function kittens(){
+  return ["Milo", "Otis", "Garfield"]
+}
+
+function destructivelyAppendKitten(name){
+  return kittens.push(name)
+}
+
+function destructivelyPrependKitten(name){
+  return kittens.unshift(name)
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
+function destructivelyRemoveFirstKitten(){
+  return kittens.shift()
+}
+
+function appendKitten(name){
+  return kittens.concat(name)
+}
+
+function prependKitten(name){
+  return [name, ...kittens]
+}
+
+function removeFirstKitten() {
+  return kittens.splice(-1)
+}
+
+function removeLastKitten(){
+  return kittens.splice(-1,1)
+}
